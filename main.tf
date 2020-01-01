6ba1ee465594bd31d8651653bd25746a2877ed82
Andrey Yuspin <yuspin@gmail.com>
2020-01-01 19:46:41 +0100

    Fixed main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,26 @@
 provider "google" {
   project = "python-258010"
-  region  = "europe-west4"
-  #region  = "us-central1"
-  zone    = "europe-west4-a"
-  #zone    = "us-central1-a"
+  region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
 
 resource "google_storage_bucket" "video-intelligence-api-bucket" {
-  location           = "EUROPE-WEST4"
   name               = "video-intelligence-api-bucket"
 }
 
 
 resource "google_cloudfunctions_function" "video-intelligence-api-function" {
-  available_memory_mb   = 128
-  trigger_http          = false
-  entry_point           = "main"
-  max_instances         = 0
   name                  = "video-intelligence-api-function"
-  region                = "europe-west4"
+  available_memory_mb   = 128
+  entry_point           = "main"
   runtime               = "python37"
-  service_account_email = "python-258010@appspot.gserviceaccount.com"
-  timeout               = 60
-
+  source_repository {
+    url = "https://source.developers.google.com/projects/python-258010/repos/video-intelligence-api-repository/moveable-aliases/master/paths/"
+  }
   event_trigger {
-     event_type = "google.storage.object.finalize"
-     resource   = "projects/_/buckets/video-intelligence-api-bucket"
-
-     failure_policy {
-         retry = false
-     }
+    event_type = "google.storage.object.finalize"
+    resource   = "projects/_/buckets/video-intelligence-api-bucket"
   }
 
 }
